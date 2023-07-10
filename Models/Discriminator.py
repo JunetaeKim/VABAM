4992c9ffcce47ae5e3affe620a9502255f8ea4b5
Junetae Kim <lyjune0070@gmail.com>
2023-07-10 19:43:37 +0900

    Losses and Models refactored

---
+++ Models/Discriminator.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, GRU, Dense,  Bidirectional 
+from tensorflow.keras import Model
+
+
+
+def EvalDiscriminator (SigDim):
+    InpL = Input(shape=(SigDim,), name='Inp_Enc')
+    InpFrame = tf.signal.frame(InpL, 100, 100)
+    DiscriLayer = Bidirectional(GRU(30, return_sequences=True))(InpFrame)
+    DiscriLayer = Bidirectional(GRU(20, return_sequences=True))(DiscriLayer)
+    DiscriLayer = Bidirectional(GRU(10, return_sequences=False))(DiscriLayer)
+    DiscriLayer = Dense(20, activation='relu')(DiscriLayer)
+    DiscriLayer = Dense(10, activation='relu')(DiscriLayer)
+    DiscriLayer = Dense(5, activation='relu')(DiscriLayer)
+    DiscriOut = Dense(1, activation='sigmoid')(DiscriLayer)
+    
+    EvalDiscModel = Model(InpL, DiscriOut, name='EvalDiscModel')
+    EvalDiscModel.compile(loss=tf.losses.binary_crossentropy, optimizer='adam')
+    
+    return EvalDiscModel
+
+
+def FacDiscriminator (LatDim, HDim):
+
+    InpL = Input(shape=(LatDim,))
+    DiscLayer = Dense(HDim, activation='softplus')(InpL)
+    DiscLayer = Dense(HDim, activation='softplus')(DiscLayer)
+    DiscLayer = Dense(HDim, activation='softplus')(DiscLayer)
+    DiscLayer = Dense(HDim, activation='softplus')(DiscLayer)
+    DiscLayer = Dense(HDim, activation='softplus')(DiscLayer)
+    DiscOut = Dense(2, activation='linear')(DiscLayer)
+    FacDiscModel = Model(InpL, DiscOut, name='FacDiscModel')
+    
+    return  FacDiscModel
--- Models/Losses.py
+++ Models/Losses.py
@@ -11,6 +11,23 @@ def LogNormalDensity(LatSamp, LatMean, LogSquaScale):
     return -0.5 * (MeanSampDiff * MeanSampDiff * InvSigma + LogSquaScale + Norm)
 
 
+
+def SplitBatch (Vec, HalfBatchIdx1, HalfBatchIdx2, mode='Both'):
+    
+    HalfBatch1 = tf.nn.embedding_lookup(Vec, HalfBatchIdx1)
+    HalfBatch2 = tf.nn.embedding_lookup(Vec, HalfBatchIdx2)
+    
+    if mode=='Both':
+        return  HalfBatch1, HalfBatch2
+    elif mode=='D1':
+        return  HalfBatch1
+    elif mode=='D2':
+        return  HalfBatch2
+    
+    
+    
+    
+    
 def TCLosses (Models, DataSize, LossConfigSet, ModelType='TCMIDKZ'):
     
     ### Loss-related parameters
@@ -132,6 +149,133 @@ def TCLosses (Models, DataSize, LossConfigSet, ModelType='TCMIDKZ'):
         SigRepModel.add_loss(kl_Loss_MI )
         SigRepModel.add_metric(kl_Loss_MI , 'kl_Loss_MI')
         print('kl_Loss_MI added')
+
+        
+    return SigRepModel
+
+
+
+
+
+def FACLosses (Models, LossConfigSet):
+    
+    ### Loss-related parameters
+    Capacity_Z = LossConfigSet['Capacity_Z']
+    Capacity_Fc = LossConfigSet['Capacity_Fc']
+    
+
+    
+    ###-------------------------------- Model structuring -------------------------------------------- ###
+    ## Model core parts
+    EncModel,FeatExtModel,FeatGenModel,ReconModel,FacDiscModel =  Models
+    EncInp =EncModel.input
+    InpZ = EncModel.output[2]
+    InpFCCommon = EncModel.output[1][:, :2]
+    InpFCEach = EncModel.output[1][:, 2:]
+
+
+    ## Batch split 
+    BatchSize = tf.shape(EncInp)[0]
+    HalfBatchIdx1 = tf.range(BatchSize//2)
+    HalfBatchIdx2 = tf.range(BatchSize//2, BatchSize)
+    Z_D1, Z_D2 = SplitBatch(InpZ, HalfBatchIdx1, HalfBatchIdx2, mode='Both')
+
+
+    ## Each output of each model
+    FeatExtOut = FeatExtModel(EncModel.output[:2])
+    FeatGenOut = FeatGenModel([InpFCCommon, InpFCEach, InpZ])
+    ReconExtOut = ReconModel(FeatExtOut)
+    ReconGenOut = ReconModel(FeatGenOut)
+    FacDiscOut_D1 =  FacDiscModel(Z_D1)
+
+    ### Define the total model
+    SigRepModel = Model(EncInp, [FacDiscOut_D1, FeatGenOut, ReconExtOut, ReconGenOut])
+
+    
+    
+    ###-------------------------------- Weights for losses -------------------------------------------- ###
+    ### Weight controller; Apply beta and capacity 
+    Beta_Z = Lossweight(name='Beta_Z', InitVal=0.01)(SigRepModel.input)
+    Beta_Fc = Lossweight(name='Beta_Fc', InitVal=0.01)(SigRepModel.input)
+    Beta_TC = Lossweight(name='Beta_TC', InitVal=0.01)(SigRepModel.input)
+    Beta_Orig = Lossweight(name='Beta_Orig', InitVal=1.)(SigRepModel.input)
+    Beta_Feat = Lossweight(name='Beta_Feat', InitVal=1.)(SigRepModel.input)
+
+
+
+    ###-------------------------------- Common losses -------------------------------------------- ###
+    ### Adding the RecLoss; 
+    MSE = tf.keras.losses.MeanSquaredError()
+    ReconExtOut_D1 = SplitBatch(ReconExtOut, HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+    EncInp_D1 = SplitBatch(EncInp, HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+    ReconOut_ext = Beta_Orig * MSE(ReconExtOut_D1, EncInp_D1)
+
+    SigRepModel.add_loss(ReconOut_ext)
+    SigRepModel.add_metric(ReconOut_ext, 'OrigRecLoss')
+    print('OrigRecLoss added')
+
+
+
+    ### Adding the FeatRecLoss; It allows connection between the extractor and generator
+    FeatGenOut_D1 = SplitBatch(tf.concat(FeatGenOut, axis=-1), HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+    FeatExtOut_D1 = SplitBatch(tf.concat(FeatExtOut, axis=-1), HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+    FeatRecLoss= Beta_Feat * MSE(FeatGenOut_D1, FeatExtOut_D1)
+
+    SigRepModel.add_loss(FeatRecLoss)
+    SigRepModel.add_metric(FeatRecLoss, 'FeatRecLoss')
+    print('FeatRecLoss added')
+
+
+
+    ### KL Divergence for p(Z) vs q(Z)
+    Z_Mu, Z_Log_Sigma, Zs = SigRepModel.get_layer('Z_Mu').output, SigRepModel.get_layer('Z_Log_Sigma').output, SigRepModel.get_layer('Zs').output
+    Z_Mu_D1 = SplitBatch(Z_Mu, HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+    Z_Log_Sigma_D1 = SplitBatch(Z_Log_Sigma, HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+
+    kl_Loss_Z = 0.5 * tf.reduce_sum( Z_Mu_D1**2  +  tf.exp(Z_Log_Sigma_D1)- Z_Log_Sigma_D1-1, axis=1)    
+    kl_Loss_Z = tf.reduce_mean(kl_Loss_Z )
+    kl_Loss_Z = Beta_Z * tf.abs(kl_Loss_Z - Capacity_Z)
+    SigRepModel.add_loss(kl_Loss_Z )
+    SigRepModel.add_metric(kl_Loss_Z , 'kl_Loss_Z')
+    print('SKZ loss selected')
+
+
+    #### ### Total Correlation # KL(q(z)||prod_j q(z_j)); log(p_true/p_false) = logit_true - logit_false
+    kl_Loss_TC = tf.reduce_mean(FacDiscOut_D1[:, 0] - FacDiscOut_D1[:, 1])
+    kl_Loss_TC = Beta_TC * kl_Loss_TC
+    SigRepModel.add_loss(kl_Loss_TC )
+    SigRepModel.add_metric(kl_Loss_TC , 'kl_Loss_TC')
+    print('kl_Loss_TC added')
+
+    
+    # Discriminator Loss
+    Batch2_Size = tf.shape(Z_D2)[0]
+    PermZ_D2 = tf.concat([tf.nn.embedding_lookup(Z_D2[:, i][:,None], tf.random.shuffle(tf.range(Batch2_Size))) for i in range(Z_D1.shape[-1])], axis=1)
+    PermZ_D2 = tf.stop_gradient(PermZ_D2) # 
+    FacDiscOut_D2 = FacDiscModel(PermZ_D2)
+
+    Ones = tf.ones_like(HalfBatchIdx1)[:,None]
+    Zeros = tf.zeros_like(HalfBatchIdx2)[:,None]
+    CCE = tf.keras.losses.MeanSquaredError()
+    kl_Loss_DTC = 0.5 * (CCE(Zeros, FacDiscOut_D1) + CCE(Ones, FacDiscOut_D2))
+    SigRepModel.add_loss(kl_Loss_DTC )
+    SigRepModel.add_metric(kl_Loss_DTC , 'kl_Loss_DTC')
+    print('kl_Loss_DTC added')
+
+
+
+    ###-------------------------------- Specific losses -------------------------------------------- ###
+    ### KL Divergence for p(FC) vs q(FC)
+    BernP = 0.5 # hyperparameter
+    FC_Mu = SigRepModel.get_layer('FC_Mu').output 
+    FC_Mu_D1 = SplitBatch(FC_Mu, HalfBatchIdx1, HalfBatchIdx2, mode='D1')
+    kl_Loss_FC = FC_Mu_D1*(tf.math.log(FC_Mu_D1) - tf.math.log(BernP)) + (1-FC_Mu_D1)*(tf.math.log(1-FC_Mu_D1) - tf.math.log(1-BernP))
+    kl_Loss_FC = tf.reduce_mean(kl_Loss_FC )
+    kl_Loss_FC = Beta_Fc * tf.abs(kl_Loss_FC - Capacity_Fc)
+    SigRepModel.add_loss(kl_Loss_FC )
+    SigRepModel.add_metric(kl_Loss_FC , 'kl_Loss_FC')
+    print('kl_Loss_FC added')
+
 
         
     return SigRepModel
