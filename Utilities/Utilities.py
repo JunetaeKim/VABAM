2b779250c7b0c1a2b5cfacd8db8a7d650a6fddcf
Junetae Kim <lyjune0070@gmail.com>
2023-07-22 17:10:29 +0900

    utilities refactored

--- Utilities/Utilities.py
+++ Utilities/Utilities.py
@@ -2,9 +2,36 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Lambda
 
 
 
+def ReName (layer, name):
+    return Lambda(lambda x: x, name=name)(layer)
+
+
+def LogNormalDensity(LatSamp, LatMean, LogSquaScale):
+    Norm = tf.math.log(2. * tf.constant(np.pi))
+    InvSigma = tf.math.exp(-LogSquaScale)
+    MeanSampDiff = (LatSamp - LatMean)
+    return -0.5 * (MeanSampDiff * MeanSampDiff * InvSigma + LogSquaScale + Norm)
+
+
+
+def SplitBatch (Vec, HalfBatchIdx1, HalfBatchIdx2, mode='Both'):
+    
+    HalfBatch1 = tf.nn.embedding_lookup(Vec, HalfBatchIdx1)
+    HalfBatch2 = tf.nn.embedding_lookup(Vec, HalfBatchIdx2)
+    
+    if mode=='Both':
+        return  HalfBatch1, HalfBatch2
+    elif mode=='D1':
+        return  HalfBatch1
+    elif mode=='D2':
+        return  HalfBatch2
+    
+
+    
 class Lossweight(tf.keras.layers.Layer):
     
     def __init__(self, InitVal = 0., name='Lossweight'):
